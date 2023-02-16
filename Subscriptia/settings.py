81e294cb1a3e5c3ef701848f69f12833e328dd0f
shadmanee <96682864+shadmanee@users.noreply.github.com>
2023-02-16 14:32:03 +0600

    commit 1 - shadmanee
    urls - product, urls - main, settings, views - index

--- Subscriptia/settings.py
+++ Subscriptia/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'product',
 ]
 
 ROOT_URLCONF = 'Subscriptia.urls'
