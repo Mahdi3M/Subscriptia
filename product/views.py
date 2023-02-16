dc361675879456d2ebb8b0e286180c147f1e77da
Tasfia Sara <tasfiasara1000@gmail.com>
2023-02-16 18:48:59 +0600

    sara5
    views.py

--- product/views.py
+++ product/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-<<<<<<< Updated upstream
-    return render(request,'base.html')
+    return render(request, 'base.html')
+
 
 def product_single(request, product_id):
     return render(request, 'product_single.html', {})
->>>>>>> Stashed changes
+
